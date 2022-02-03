9a1d9807b7d89008ca84f54bcfcd759d3a9b57b3
ZH_ubuntu <myonly2015@163.com>
2022-02-03 20:11:10 +0800

    V2: add simple stream viewer(new window for simple camera)

--- AstraSDK-Ubuntu16.04-x86_64/samples/sfml/SimpleBodyViewer-SFML/main.cpp
+++ AstraSDK-Ubuntu16.04-x86_64/samples/sfml/SimpleBodyViewer-SFML/main.cpp
@@ -28,8 +28,15 @@
 #include <stdbool.h>
 #include <key_handler.h>
 
+//include for SimipleStreamViewer
+#include <astra_core/astra_core.hpp>
+#include "LitDepthVisualizer.hpp"
+#include <chrono>
+#include <iomanip>
+
+
 /*
-code for BodyReaderPull below
+code below is for BodyReaderPull 
 */
 void output_floor(astra_bodyframe_t bodyFrame)
 {
@@ -151,10 +158,10 @@ void output_hand_poses(const astra_body_t* body)
     const astra_handpose_t leftHandPose = handPoses->leftHand;
     const astra_handpose_t rightHandPose = handPoses->rightHand;
 
-    printf("Body %d Left hand pose: %d Right hand pose: %d\n",
-        body->id,
-        leftHandPose,
-        rightHandPose);
+    // printf("Body %d Left hand pose: %d Right hand pose: %d\n",
+    //     body->id,
+    //     leftHandPose,
+    //     rightHandPose);
 }
 
 void output_bodies(astra_bodyframe_t bodyFrame)
@@ -201,11 +208,11 @@ void output_bodies(astra_bodyframe_t bodyFrame)
             const bool jointTrackingEnabled       = (features & ASTRA_BODY_TRACKING_JOINTS)     == ASTRA_BODY_TRACKING_JOINTS;
             const bool handPoseRecognitionEnabled = (features & ASTRA_BODY_TRACKING_HAND_POSES) == ASTRA_BODY_TRACKING_HAND_POSES;
 
-            printf("Body %d CenterOfMass (%f, %f, %f) Joint Tracking Enabled: %s Hand Pose Recognition Enabled: %s\n",
-                bodyId,
-                centerOfMass->x, centerOfMass->y, centerOfMass->z,
-                jointTrackingEnabled       ? "True" : "False",
-                handPoseRecognitionEnabled ? "True" : "False");
+            // printf("Body %d CenterOfMass (%f, %f, %f) Joint Tracking Enabled: %s Hand Pose Recognition Enabled: %s\n",
+            //     bodyId,
+            //     centerOfMass->x, centerOfMass->y, centerOfMass->z,
+            //     jointTrackingEnabled       ? "True" : "False",
+            //     handPoseRecognitionEnabled ? "True" : "False");
 
             const astra_joint_t* joint = &body->joints[ASTRA_JOINT_RIGHT_HAND];
 
@@ -388,7 +395,7 @@ public:
         lastTimepoint_ = newTimepoint;
         double fps = 1.0 / frameDuration_;
 
-        printf("FPS: %3.1f (%3.4Lf ms)\n", fps, frameDuration_ * 1000);
+        // printf("FPS: %3.1f (%3.4Lf ms)\n", fps, frameDuration_ * 1000);
     }
 
     void processDepth(astra::Frame& frame)
@@ -445,12 +452,12 @@ public:
 
         for (auto& body : bodies)
         {
-            printf("Processing frame #%d body %d right hand: %u\n",
-                bodyFrame.frame_index(), body.id(), unsigned(body.hand_poses().right_hand()));
+            // printf("Processing frame #%d body %d right hand: %u\n",
+                // bodyFrame.frame_index(), body.id(), unsigned(body.hand_poses().right_hand()));
             for(auto& joint : body.joints())
             {
                 jointPositions_.push_back(joint.depth_position());
-                printf("depth_position : ", joint.depth_position());
+                // printf("depth_position : ", joint.depth_position());
             }
 
             update_body(body, jointScale);
@@ -460,9 +467,9 @@ public:
         if (floor.floor_detected())
         {
             const auto& p = floor.floor_plane();
-            std::cout << "Floor plane: ["
-                << p.a() << ", " << p.b() << ", " << p.c() << ", " << p.d()
-                << "]" << std::endl;
+            // std::cout << "Floor plane: ["
+            //     << p.a() << ", " << p.b() << ", " << p.c() << ", " << p.d()
+            //     << "]" << std::endl;
 
         }
 
@@ -812,6 +819,368 @@ astra::DepthStream configure_depth(astra::StreamReader& reader)
     return depthStream;
 }
 
+/*
+code below is for simple stream viewer
+*/
+enum ColorMode
+    {
+        MODE_COLOR,
+        MODE_IR_16,
+        MODE_IR_RGB,
+    };
+class MultiFrameListener : public astra::FrameListener
+{
+public:
+    using BufferPtr = std::unique_ptr<uint8_t[]>;
+
+    struct StreamView
+    {
+        sf::Sprite sprite_;
+        sf::Texture texture_;
+        BufferPtr buffer_;
+        int width_{0};
+        int height_{0};
+    };
+
+    MultiFrameListener()
+    {
+        prev_ = ClockType::now();
+    }
+
+    void init_texture(int width, int height, StreamView& view)
+    {
+        if (view.buffer_ == nullptr || width != view.width_ || height != view.height_)
+        {
+            view.width_ = width;
+            view.height_ = height;
+
+            // texture is RGBA
+            const int byteLength = width * height * 4;
+
+            view.buffer_ = BufferPtr(new uint8_t[byteLength]);
+
+            clear_view(view);
+
+            view.texture_.create(width, height);
+            view.sprite_.setTexture(view.texture_, true);
+            view.sprite_.setPosition(0, 0);
+        }
+    }
+
+    void clear_view(StreamView& view)
+    {
+        const int byteLength = view.width_ * view.height_ * 4;
+        std::fill(&view.buffer_[0], &view.buffer_[0] + byteLength, 0);
+    }
+
+    void check_fps()
+    {
+        const float frameWeight = .2f;
+
+        const ClockType::time_point now = ClockType::now();
+        const float elapsedMillis = std::chrono::duration_cast<DurationType>(now - prev_).count();
+
+        elapsedMillis_ = elapsedMillis * frameWeight + elapsedMillis_ * (1.f - frameWeight);
+        prev_ = now;
+
+        const float fps = 1000.f / elapsedMillis;
+
+        const auto precision = std::cout.precision();
+
+        std::cout << std::fixed
+                  << std::setprecision(1)
+                  << fps << " fps ("
+                  << std::setprecision(1)
+                  << elapsedMillis_ << " ms)"
+                  << std::setprecision(precision)
+                  << std::endl;
+    }
+
+    void update_depth(astra::Frame& frame)
+    {
+        const astra::PointFrame pointFrame = frame.get<astra::PointFrame>();
+
+        if (!pointFrame.is_valid())
+        {
+            clear_view(depthView_);
+            depthView_.texture_.update(&depthView_.buffer_[0]);
+            return;
+        }
+
+        const int depthWidth = pointFrame.width();
+        const int depthHeight = pointFrame.height();
+
+        init_texture(depthWidth, depthHeight, depthView_);
+
+        if (isPaused_) { return; }
+
+        visualizer_.update(pointFrame);
+
+        astra::RgbPixel* vizBuffer = visualizer_.get_output();
+        uint8_t* buffer = &depthView_.buffer_[0];
+        for (int i = 0; i < depthWidth * depthHeight; i++)
+        {
+            const int rgbaOffset = i * 4;
+            buffer[rgbaOffset] = vizBuffer[i].r;
+            buffer[rgbaOffset + 1] = vizBuffer[i].g;
+            buffer[rgbaOffset + 2] = vizBuffer[i].b;
+            buffer[rgbaOffset + 3] = 255;
+        }
+
+        depthView_.texture_.update(&depthView_.buffer_[0]);
+    }
+
+    void update_color(astra::Frame& frame)
+    {
+        const astra::ColorFrame colorFrame = frame.get<astra::ColorFrame>();
+
+        if (!colorFrame.is_valid())
+        {
+            clear_view(colorView_);
+            colorView_.texture_.update(&colorView_.buffer_[0]);
+            return;
+        }
+
+        const int colorWidth = colorFrame.width();
+        const int colorHeight = colorFrame.height();
+
+        init_texture(colorWidth, colorHeight, colorView_);
+
+        if (isPaused_) { return; }
+
+        const astra::RgbPixel* color = colorFrame.data();
+        uint8_t* buffer = &colorView_.buffer_[0];
+
+        for(int i = 0; i < colorWidth * colorHeight; i++)
+        {
+            const int rgbaOffset = i * 4;
+            buffer[rgbaOffset] = color[i].r;
+            buffer[rgbaOffset + 1] = color[i].g;
+            buffer[rgbaOffset + 2] = color[i].b;
+            buffer[rgbaOffset + 3] = 255;
+        }
+
+        colorView_.texture_.update(&colorView_.buffer_[0]);
+    }
+
+    void update_ir_16(astra::Frame& frame)
+    {
+        const astra::InfraredFrame16 irFrame = frame.get<astra::InfraredFrame16>();
+
+        if (!irFrame.is_valid())
+        {
+            clear_view(colorView_);
+            colorView_.texture_.update(&colorView_.buffer_[0]);
+            return;
+        }
+
+        const int irWidth = irFrame.width();
+        const int irHeight = irFrame.height();
+
+        init_texture(irWidth, irHeight, colorView_);
+
+        if (isPaused_) { return; }
+
+        const uint16_t* ir_values = irFrame.data();
+        uint8_t* buffer = &colorView_.buffer_[0];
+        for (int i = 0; i < irWidth * irHeight; i++)
+        {
+            const int rgbaOffset = i * 4;
+            const uint16_t value = ir_values[i];
+            const uint8_t red = static_cast<uint8_t>(value >> 2);
+            const uint8_t blue = 0x66 - red / 2;
+            buffer[rgbaOffset] = red;
+            buffer[rgbaOffset + 1] = 0;
+            buffer[rgbaOffset + 2] = blue;
+            buffer[rgbaOffset + 3] = 255;
+        }
+
+        colorView_.texture_.update(&colorView_.buffer_[0]);
+    }
+
+    void update_ir_rgb(astra::Frame& frame)
+    {
+        const astra::InfraredFrameRgb irFrame = frame.get<astra::InfraredFrameRgb>();
+
+        if (!irFrame.is_valid())
+        {
+            clear_view(colorView_);
+            colorView_.texture_.update(&colorView_.buffer_[0]);
+            return;
+        }
+
+        int irWidth = irFrame.width();
+        int irHeight = irFrame.height();
+
+        init_texture(irWidth, irHeight, colorView_);
+
+        if (isPaused_) { return; }
+
+        const astra::RgbPixel* irRGB = irFrame.data();
+        uint8_t* buffer = &colorView_.buffer_[0];
+        for (int i = 0; i < irWidth * irHeight; i++)
+        {
+            const int rgbaOffset = i * 4;
+            buffer[rgbaOffset] = irRGB[i].r;
+            buffer[rgbaOffset + 1] = irRGB[i].g;
+            buffer[rgbaOffset + 2] = irRGB[i].b;
+            buffer[rgbaOffset + 3] = 255;
+        }
+
+        colorView_.texture_.update(&colorView_.buffer_[0]);
+    }
+
+    virtual void on_frame_ready(astra::StreamReader& reader,
+                                astra::Frame& frame) override
+    {
+        update_depth(frame);
+
+        switch (colorMode_)
+        {
+        case MODE_COLOR:
+            update_color(frame);
+            break;
+        case MODE_IR_16:
+            update_ir_16(frame);
+            break;
+        case MODE_IR_RGB:
+            update_ir_rgb(frame);
+            break;
+        }
+
+        check_fps();
+    }
+
+    void draw_to(sf::RenderWindow& window, sf::Vector2f origin, sf::Vector2f size)
+    {
+        const int viewSize = (int)(size.x / 2.0f);
+        const sf::Vector2f windowSize = window.getView().getSize();
+
+        if (depthView_.buffer_ != nullptr)
+        {
+            const float depthScale = viewSize / (float)depthView_.width_;
+            const int horzCenter = origin.y * windowSize.y;
+
+            depthView_.sprite_.setScale(depthScale, depthScale);
+            depthView_.sprite_.setPosition(origin.x * windowSize.x, horzCenter);
+            window.draw(depthView_.sprite_);
+        }
+
+        if (colorView_.buffer_ != nullptr)
+        {
+            const float colorScale = viewSize / (float)colorView_.width_;
+            const int horzCenter = origin.y * windowSize.y;
+
+            colorView_.sprite_.setScale(colorScale, colorScale);
+
+            if (overlayDepth_)
+            {
+                colorView_.sprite_.setPosition(origin.x * windowSize.x, horzCenter);
+                colorView_.sprite_.setColor(sf::Color(255, 255, 255, 128));
+            }
+            else
+            {
+                colorView_.sprite_.setPosition(origin.x * windowSize.x + viewSize, horzCenter);
+                colorView_.sprite_.setColor(sf::Color(255, 255, 255, 255));
+            }
+
+            window.draw(colorView_.sprite_);
+        }
+    }
+
+    void toggle_depth_overlay()
+    {
+        overlayDepth_ = !overlayDepth_;
+    }
+
+    bool get_overlay_depth() const
+    {
+        return overlayDepth_;
+    }
+
+    void toggle_paused()
+    {
+        isPaused_ = !isPaused_;
+    }
+
+    bool is_paused() const
+    {
+        return isPaused_;
+    }
+
+    ColorMode get_mode() const { return colorMode_; }
+    void set_mode(ColorMode mode) { colorMode_ = mode; }
+
+private:
+    samples::common::LitDepthVisualizer visualizer_;
+
+    using DurationType = std::chrono::milliseconds;
+    using ClockType = std::chrono::high_resolution_clock;
+
+    ClockType::time_point prev_;
+    float elapsedMillis_{.0f};
+
+    StreamView depthView_;
+    StreamView colorView_;
+    ColorMode colorMode_;
+    bool overlayDepth_{ false };
+    bool isPaused_{ false };
+};
+
+astra::InfraredStream configure_ir(astra::StreamReader& reader, bool useRGB)
+{
+    auto irStream = reader.stream<astra::InfraredStream>();
+
+    auto oldMode = irStream.mode();
+
+    astra::ImageStreamMode irMode;
+    irMode.set_width(640);
+    irMode.set_height(480);
+
+    if (useRGB)
+    {
+        irMode.set_pixel_format(astra_pixel_formats::ASTRA_PIXEL_FORMAT_RGB888);
+    }
+    else
+    {
+        irMode.set_pixel_format(astra_pixel_formats::ASTRA_PIXEL_FORMAT_GRAY16);
+    }
+
+    irMode.set_fps(30);
+    irStream.set_mode(irMode);
+
+    auto newMode = irStream.mode();
+    printf("Changed IR mode: %dx%d @ %d -> %dx%d @ %d\n",
+        oldMode.width(), oldMode.height(), oldMode.fps(),
+        newMode.width(), newMode.height(), newMode.fps());
+
+    return irStream;
+}
+
+astra::ColorStream configure_color(astra::StreamReader& reader)
+{
+    auto colorStream = reader.stream<astra::ColorStream>();
+
+    auto oldMode = colorStream.mode();
+
+    astra::ImageStreamMode colorMode;
+    colorMode.set_width(640);
+    colorMode.set_height(480);
+    colorMode.set_pixel_format(astra_pixel_formats::ASTRA_PIXEL_FORMAT_RGB888);
+    colorMode.set_fps(30);
+
+    colorStream.set_mode(colorMode);
+
+    auto newMode = colorStream.mode();
+    printf("Changed color mode: %dx%d @ %d -> %dx%d @ %d\n",
+        oldMode.width(), oldMode.height(), oldMode.fps(),
+        newMode.width(), newMode.height(), newMode.fps());
+
+    return colorStream;
+}
+/* **** gap line **** */
+
+
 int main(int argc, char** argv)
 {
     astra::initialize();
@@ -841,6 +1210,28 @@ int main(int argc, char** argv)
 
     const sf::VideoMode fullScreenMode = sf::VideoMode::getFullscreenModes()[0];
     const sf::VideoMode windowedMode(1280, 960);
+
+    /*
+    code below is for simple stream viewer
+    */
+    sf::RenderWindow window1(windowedMode, "Stream Viewer");
+    astra::StreamSet streamSet;
+    astra::StreamReader reader_stream = streamSet.create_reader();
+
+    reader_stream.stream<astra::PointStream>().start();
+
+    auto colorStream = configure_color(reader_stream);
+    colorStream.start();
+
+    auto irStream = configure_ir(reader_stream, false);
+
+    MultiFrameListener listener_stream;
+    listener_stream.set_mode(MODE_COLOR);
+
+    reader_stream.add_listener(listener_stream);
+    /* **** gap line **** */
+
+
     bool isFullScreen = false;
 
     astra::StreamSet sensor;
@@ -1025,6 +1416,10 @@ int main(int argc, char** argv)
 
         listener.draw_to(window);
         window.display();
+
+        window1.clear(sf::Color::Black);
+        listener_stream.draw_to(window1, sf::Vector2f(0.f, 0.f), sf::Vector2f(window1.getSize().x, window1.getSize().y));
+        window1.display();
     }
 
     astra::terminate();
